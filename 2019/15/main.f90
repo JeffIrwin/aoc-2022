c749b02cfb3b396916ca1124cb52205d274505d5
irwinj <jeff.irwin@autodesk.com>
2022-12-31 20:43:47 -0500

    Add 2019 day 15 part2

--- 2019/15/main.f90
+++ 2019/15/main.f90
@@ -28,20 +28,26 @@ module m
 	! Droid status codes
 	integer, parameter :: wall = 0, moved = 1, found = 2
 
+	integer, allocatable :: map(:,:)
+	integer :: oxy_steps, oxy_x, oxy_y, stepsmax
+
+	integer, parameter :: qmax = 1024, xmax = 512
+
 contains
 
 !===============================================================================
 
 subroutine bfs1(prog0)
 
+	! Breadth-first-search first pass:  the map is unknown.  Explore and chart the
+	! map by querying the droid status
+
 	integer(kind = ick) :: prog0(:)
 
 	!********
 
-	integer, parameter :: qmax = 1024, xmax = 512
-
-	integer :: ql, qr, x, y, u(nd), v(nd), w(nd), iw, i
-	integer, allocatable :: q(:,:), parent(:,:)
+	integer :: ql, qr, x, y, u(nd), v(nd), w(nd), iw, i, j, stat
+	integer, allocatable :: q(:,:), parent(:,:), dist(:,:)
 	integer(kind = ick), allocatable :: moves(:)
 
 	logical, allocatable :: l(:,:)
@@ -58,14 +64,23 @@ subroutine bfs1(prog0)
 	allocate(parent(-xmax: xmax, -xmax: xmax))
 	parent = -1
 
+	allocate(dist(-xmax: xmax, -xmax: xmax))
+	dist = -1
+
+	if (allocated(map)) deallocate(map)
+	allocate(map(-xmax: xmax, -xmax: xmax))
+	map = wall
+
 	! Begin search at origin as root
 	x = 0
 	y = 0
 
-	! Label root as explored 
+	! Label root as explored.  Assume we don't start on a wall
 	l(x,y) = .true.
+	map(x,y) = moved
+	dist(x,y) = 0
 
-	! Enqueue the root (on the right end of the queue)
+	! Initialize and enqueue the root (on the right end of the queue)
 	ql = 1
 	qr = 1
 	q(:,qr) = [x,y]
@@ -89,13 +104,24 @@ subroutine bfs1(prog0)
 
 				! Retrace the steps to get to w from last move to first.  This is
 				! a terrible way to push things to an array, it amortizes every time,
-				! but idgaf
-				moves = [iw]
+				! but idgaf.  It wouldn't be too hard to make another array that tracks
+				! the distance of each point from start to help pre-allocate here
 				u = v
-				do while (parent(u(1),u(2)) > 0)
-					moves = [int(parent(u(1),u(2)),ick), moves]
+				dist(w(1), w(2)) = dist(u(1), u(2)) + 1
+				j = dist(w(1), w(2))
+				allocate(moves(j))
+				moves(j) = iw
+				do while (parent(u(1), u(2)) > 0)
+					j = j - 1
+					moves(j) =      parent(u(1), u(2))
 					u = u - dirs(:, parent(u(1),u(2)))
 				end do
+
+				!moves = [iw]
+				!do while (parent(u(1),u(2)) > 0)
+				!	moves = [int(parent(u(1),u(2)),ick), moves]
+				!	u = u - dirs(:, parent(u(1),u(2)))
+				!end do
 
 				!print *, 'w = ', w
 				!print *, 'moves = ', moves
@@ -106,17 +132,28 @@ subroutine bfs1(prog0)
 				! Restart the interpreter from the beginning for each location
 				ic = new(prog0, moves)
 				call ic%interpret()
-				!print *, 'out = ', ic%outputs(0: ic%io-1)
+				stat = ic%outputs(ic%io-1)
+
+				deallocate(moves)
+
+				!print *, 'stat = ', stat
 				!print *, ''
+
+				map(w(1), w(2)) = stat
 
 				! Stop if we hit a wall on the last move.  We don't need to check
 				! earlier moves because they wouldn't have been enqueued
-				if (ic%outputs(ic%io-1) == wall) cycle
+				if (stat == wall) cycle
 
 				! Return when oxygen found
-				if (ic%outputs(ic%io-1) == found) then
-					write(*,*) 'part1 = ', size(moves)
-					return
+				if (stat == found) then
+					!write(*,*) 'part1 = ', size(moves)
+					!print *, 'i = ', i
+					!oxy_steps = size(moves)
+					oxy_steps = dist(w(1), w(2))
+					oxy_x = w(1)
+					oxy_y = w(2)
+					!return
 				end if
 
 				! Parent stores the move dir index to get from v to w
@@ -136,6 +173,120 @@ end subroutine bfs1
 
 !===============================================================================
 
+subroutine bfs2()
+
+	! Breadth-first-search second pass:  the map is now known after being set by
+	! bf1().  We can search from any starting point without having to interpret
+	! Intcode.
+	!
+	! There's probably a cleaner way of doing this than having 2 copies of BFS,
+	! but idgaf.
+
+	integer :: ql, qr, x, y, u(nd), v(nd), w(nd), iw, i, stat, steps
+	integer, allocatable :: q(:,:), parent(:,:)
+	integer(kind = ick), allocatable :: moves(:)
+
+	logical, allocatable :: l(:,:)
+
+	type(intcode) :: ic
+
+	!print *, 'starting bfs2()'
+	!print *, 'oxy xy = ', oxy_x, oxy_y
+	!print *, ''
+
+	! Queue
+	allocate(q(nd, qmax))
+
+	! Visited locations
+	allocate(l(-xmax: xmax, -xmax: xmax))
+	l = .false.
+
+	! Is this required for part2?
+	allocate(parent(-xmax: xmax, -xmax: xmax))
+	parent = -1
+
+	! Begin search at oxy location found by bfs1()
+	x = oxy_x
+	y = oxy_y
+
+	! Label root as explored 
+	l(x,y) = .true.
+
+	! Initialize and enqueue the root (on the right end of the queue)
+	ql = 1
+	qr = 1
+	q(:,qr) = [x,y]
+
+	stepsmax = -huge(stepsmax)
+
+	! While queue is not empty
+	i = 0
+	do while (ql <= qr)
+		i = i + 1
+
+		! Dequeue v from q (from left end)
+		v = q(:,ql)
+		ql = ql + 1
+
+		! For all edges from v to w
+		do iw = 1, ndirs
+
+			w = v + dirs(:,iw)
+
+			if (.not. l(w(1), w(2)) .and. .not. map(w(1), w(2)) == wall) then
+				! w is not explored yet
+
+				! Retrace the steps to get to w from last move to first, just to count
+				! the distance from oxy to current loc
+				u = v
+				steps = 1
+				do while (parent(u(1),u(2)) > 0)
+					steps = steps + 1
+					u = u - dirs(:, parent(u(1),u(2)))
+				end do
+				stepsmax = max(stepsmax, steps)
+
+				!print *, 'xy, steps = ', w, steps
+
+				! Label w as explored
+				l(w(1), w(2)) = .true.
+
+				! Parent stores the move dir index to get from v to w
+				parent(w(1), w(2)) = iw
+
+				! Enqueue w
+				qr = qr + 1
+				q(:,qr) = [w(1), w(2)]
+
+			end if
+
+		end do
+
+	end do
+
+	!! This is not the answer
+	!print *, 'i = ', i
+
+end subroutine bfs2
+
+!===============================================================================
+
+subroutine part2()
+
+	integer(kind = ick), allocatable :: prog(:)
+
+	prog = readprog(finput)
+
+	call bfs1(prog)
+	call bfs2()
+
+	write(*,*) 'part2 = ', stepsmax
+	write(*,*) ''
+
+end subroutine part2
+
+!===============================================================================
+
 subroutine part1()
 
 	integer(kind = ick), allocatable :: prog(:)
@@ -144,8 +295,7 @@ subroutine part1()
 
 	call bfs1(prog)
 
-	!! TODO: get a return val from bfs1
-	!write(*,*) 'part1 = ', ic%outputs(0: ic%io-1)
+	write(*,*) 'part1 = ', oxy_steps
 	write(*,*) ''
 
 end subroutine part1
@@ -165,7 +315,7 @@ program main
 	write(*,*) ''
 
 	call part1()
-	!call part2()
+	call part2()
 
 	write(*,*) 'Ending AOC main'
 	write(*,*) ''
