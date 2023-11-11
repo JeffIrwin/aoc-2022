18cb982df4e0d467ef90a4ec04ed59de8a1eeb37
Jeff Irwin <jirwin505@gmail.com>
2023-11-11 13:18:28 -0500

    add 2020 day 1 part 2

--- 2020/1/main.f90
+++ 2020/1/main.f90
@@ -25,7 +25,7 @@ subroutine part1()
 	integer, allocatable :: vec(:)
 
 	n = countlines(finput)
-	print *, 'n = ', n
+	!print *, 'n = ', n
 
 	allocate(vec(n))
 
@@ -65,6 +65,57 @@ end subroutine part1
 
 !===============================================================================
 
+subroutine part2()
+
+	character :: s*256
+
+	integer :: i, j, k, iu, io, isum, n, is
+	integer, allocatable :: vec(:)
+
+	n = countlines(finput)
+	!print *, 'n = ', n
+
+	allocate(vec(n))
+
+	open(file = finput, newunit = iu, status = 'old')
+
+	isum = 0
+
+	i = 0
+	do
+		read(iu, '(a)', iostat = io) s
+		if (io == iostat_end) exit
+
+		!print *, 's = ', trim(s)
+
+		i = i + 1
+		is = 1
+		vec(i) = readint(s, is)
+
+	end do
+
+	close(iu)
+
+	!print *, 'vec = ', vec
+
+	do i = 3, n
+	do j = 1, i - 1
+	do k = 1, j - 1
+		if (sum(vec([i,j,k])) == 2020) then
+			!isum = vec(i) * vec(j) * vec(k)
+			isum = product(vec([i,j,k]))
+		end if
+	end do
+	end do
+	end do
+
+	write(*,*) 'part2 = ', isum
+	write(*,*) ''
+
+end subroutine part2
+
+!===============================================================================
+
 end module m
 
 !===============================================================================
@@ -78,7 +129,7 @@ program main
 	write(*,*) ''
 
 	call part1()
-	!call part2()
+	call part2()
 
 	write(*,*) 'Ending AOC main'
 	write(*,*) ''
